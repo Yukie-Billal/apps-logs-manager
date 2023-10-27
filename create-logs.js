6fa57e2a572a36fd4d0e626364dfc2e7d65d0c69
Yukie M Billal <yukiembillal01@gmail.com>
2023-10-27 07:04:57 +0700

    feat: endopint save logs & fix error create-logs.js

--- create-logs.js
+++ create-logs.js
@@ -13,17 +13,17 @@ if (!fs.existsSync(filePath)) {
 const getLogs = () => JSON.parse(fs.readFileSync("data/logs.json", "utf-8"))
 
 export const saveLogs = (date, log, category) => {
-   let logs = getContacts()
+   let logs = getLogs()
    if (!category) {
       console.log(
-         write("Missing field categori", 'error')
+         "Missing field categori", 'error'
       )
       return false
    }
 
    logs.push({ date, log, category })
    fs.writeFile("data/logs.json", JSON.stringify(logs), () => { })
-   console.log(write(`Terima kasih, Logs tersimpan ${date}`, 'success'))
+   console.log(`Terima kasih, Logs tersimpan ${date}`, 'success')
 }
 
 export const getAllLogs = () => getLogs()
--- routes/logs.js
+++ routes/logs.js
@@ -2,11 +2,16 @@ import {Router} from 'express'
 
 const router = Router()
 
-import {getAllLogs} from '../create-logs.js'
+import {getAllLogs, saveLogs} from '../create-logs.js'
 
 router.get('/', (req, res) => {
     res.json(getAllLogs())
 })
 
+router.post('/', (req, res) => {
+    const {date, log, category} = req.body
+    saveLogs(date, log, category)
+    res.json({date, log, category}).status(200)
+})
 
 export default router
