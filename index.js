d8b3f3d062d9695e7fe19ddcebcfc01d970124aa
Yukie Muhammad Billal <114663852+Yukie-Billal@users.noreply.github.com>
2023-10-27 07:11:12 +0700

    feat: api endpoint get & save logs (#4)
    * feat: simple api
    
    * feat: get & save log func
    
    * feat: endpoint read logs from logs.json file
    
    * feat: endopint save logs & fix error create-logs.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.get('/', (req, res) => {
   res.json({"message": "hello World"})
 })
 
+import LogsRouter from './routes/logs.js'
+app.use('/logs', LogsRouter)
+
 app.listen(5000, "0.0.0.0", () => {
    console.log("run on port 5000")
 })
