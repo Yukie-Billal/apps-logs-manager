b83ccb3858f6ae6fd9cf91fa83542865aca4b302
Yukie M Billal <yukiembillal01@gmail.com>
2023-11-01 23:14:00 +0700

    fix(api): error handler & refactor

--- routes/logs.js
+++ routes/logs.js
@@ -1,21 +1,32 @@
 import {Router} from 'express'
-
 const router = Router()
 
 import {getAllLogs, removeLog, saveLogs} from '../utils/logs.js'
+
 router.get('/', (req, res) => {
-    res.json(getAllLogs())
+    res.status(200).json(getAllLogs())
 })
 
 router.post('/', (req, res) => {
-    const {urgent, date= new Date('2023 10 24 11:42:28'), log, category} = req.body
-    saveLogs(urgent, date, log, category)
-    res.json({urgent, date, log, category}).status(200)
+    try {
+        const {urgent, date= new Date('2023 10 24 11:42:28'), log, category} = req.body
+        if (!urgent || !log || !category) {
+            res.status(400).json({message: 'Invalid parameter'})
+            return false
+        }
+        saveLogs(urgent, date, log, category)
+        res.status(200).json({urgent, date, log, category})
+    } catch (e) {
+        res.status(500).json({"message": "Internal server error"})
+    }
 })
 
 router.delete('/:id', (req, res) => {
-    const id = req.params.id
-    res.send(removeLog(id))
+    try {
+        res.status(200).json({message: removeLog(req.params.id)})
+    } catch (e) {
+        res.status(500).json({"message": "Internal server error"})
+    }
 })
 
 export default router
