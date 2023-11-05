e70c5b31bf84f4b3c76d2af757b72bbf1675600b
Yukie Muhammad Billal <114663852+Yukie-Billal@users.noreply.github.com>
2023-11-05 19:20:10 +0700

    feat: simple login logic in web view (#17)

--- routes/web.js
+++ routes/web.js
@@ -1,11 +1,26 @@
 import {Router} from 'express'
 import fs from "fs";
-import {removeLog} from "../utils/logs.js";
 const router = Router()
 
 router.get('/', (req, res) => {
+  const isi = fs.readFileSync('./views/login.html', 'utf-8')
+  res.send(isi)
+})
+
+router.get('/logs-view', (req, res) => {
   const isi = fs.readFileSync('./views/index.html', 'utf-8')
   res.send(isi)
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  const {username, password} = req.body
+  if (!username || !password) return res.status(400).json({message: "error"})
+  if (username==="admin" && password==="admin") {
+    res.json({"message":"success"})
+    return true
+  }
+  res.status(400).json({"message":"failed"})
 })
 
 export default router
